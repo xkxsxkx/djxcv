661b926c446a5cb4c60b102c8b58915cdf393c54
瑞 王 <1042460192@qq.com>
2021-04-02 16:25:14 +0800

    first commit

---
+++ pyxcv_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PyxcvAppConfig(AppConfig):
+    name = 'pyxcv_app'
---
+++ pyxcv_app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+def makeTemplate(request):
+    pass
+
+def matchTemplate(request):
+    pass
+
+def prevTemplate(request):
+    pass
